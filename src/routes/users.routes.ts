873737eac5ad27b24a7d2fb94e994dd86aaa6b7a
Rodrigo Andrade <rodrigoandradebcc@gmail.com>
2020-09-26 03:04:59 -0300

    Validador de CPF

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,5 +1,9 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
 import { Router } from 'express';
+
 import User from '../models/User';
+
+const cpfValidator = require('cpf-val');
 
 const usersRouter = Router();
 
@@ -7,6 +11,10 @@ const users: User[] = [];
 
 usersRouter.post('/', (request, response) => {
   const { name, email, maritalStatus, cpf, city, state } = request.body;
+
+  if (!cpfValidator(cpf)) {
+    return response.status(400).json({ message: 'Esse cpf é inválido' });
+  }
 
   const user = new User(name, email, maritalStatus, cpf, city, state);
 
